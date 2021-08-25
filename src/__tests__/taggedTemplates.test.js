13a2da7e4930aae59a54bac3188a240ae06de571
Anastasia Shmeleva <shmeleva-nastya@mail.ru>
2021-08-25 12:25:53 +0300

    branch fix

--- src/__tests__/taggedTemplates.test.js
+++
@@ -1,7 +0,0 @@
-test('tagged template', () => {
-  const level = 1;
-  const attack = 25;
-  const defence = 25;
-  const health = 50;
-  expect(`🎖 ${level} ⚔ ${attack} 🛡 ${defence} ❤ ${health}`).toBe('🎖 1 ⚔ 25 🛡 25 ❤ 50');
-});
--- src/js/Team.js
+++
@@ -1,8 +0,0 @@
-export default class Team {
-  constructor() {
-    this.team = [];
-  }
-}
-
-export const playerTeam = new Team();
-export const computerTeam = new Team();
